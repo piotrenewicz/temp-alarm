a80b491c232891062c24ed46f60f74c3ba6fadaf
Piotrenewicz <piotrekm9@onet.eu>
2022-01-08 19:33:43 +0100

    minor cleanup

--- inveo_get.py
+++ inveo_get.py
@@ -1,6 +1,7 @@
 import requests
 
+
 def get_data(ip: str):
-    url = "http://{0}/temp1.txt".format(ip)
+    url = "https://{0}/temp1.txt".format(ip)
     response = requests.request("GET", url)
     return float(response.text)
