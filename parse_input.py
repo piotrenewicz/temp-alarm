6731468a5b8320d8dbfe4b4511c54025b3ae31a0
Mykhailo Marchenko <mykm@opinov8.com>
2022-01-23 18:18:41 +0200

    Middle-work draft: main file, mail sending rework, input parse addition

--- parse_input.py
+++ parse_input.py
@@ -5,7 +5,7 @@ INPUT_FILE_NAME = "input.json"
 INPUT_SCHEMA_FILE_NAME = "input_schema.json"
 
 
-def validate_schema(input_object):
+def validate_schema(input_object: dict) -> None:
     with open(INPUT_SCHEMA_FILE_NAME, 'r') as file:
         schema = json.load(file)
 
@@ -14,10 +14,18 @@ def validate_schema(input_object):
     return None
 
 
-def get_input():
+def get_input() -> dict:
     with open(INPUT_FILE_NAME) as f:
-        input_json = json.load(f)
+        input_object = json.load(f)
 
-    validate_schema(input_json)  # Validate the input
+    validate_schema(input_object)  # Validate the input
 
-    return input_json
+    return input_object
+
+
+def get_split_input() -> "tuple[list,list]":
+    input_object = get_input()
+    sensors = input_object["sensors"]
+    mail_list = input_object["send_alarm_to_mails"]
+
+    return sensors, mail_list
